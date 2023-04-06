2841bafb2dcb607f7996acf126e0b5b9dcdba6c9
phillip-che <pche@cpp.edu>
2023-04-05 23:29:07 -0700

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HomePage from './components/HomePage'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <div className="App">
       <HomePage />
+      <SideBar />
     </div>
   )
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-item">Home</div>
+            <div className="sidebar-item">Create Crewmate</div>
+            <div className="sidebar-item">Crewmate Gallery</div>
+        </div>
+    )
+}
+
+export default SideBar
